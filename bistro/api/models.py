d2b86dae3194bcb249ab93df62c84b604329aa60
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-09 16:57:27 +0000

    added models

--- bistro/api/models.py
+++ bistro/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu_item(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Cuisine(models.Model):
+    title = models.CharField(max_length=255)
