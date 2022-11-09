2df8a758e128ebde90550a1aad8188c3f916ab37
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-09 21:31:50 +0000

    added spice level

--- bistro/api/views.py
+++ bistro/api/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from api.models import Menu_item
+from api.models import Menu_item, Category, Cuisine
+from pprint import pprint as pp
 
 # Create your views here.
+
+
 def getData(request):
-    data = list(Menu_item.objects.values())
-    return JsonResponse({'data':data})
+    if request.method == 'GET':
+
+        category = Category.objects.values()
+        cuisine = Cuisine.objects.values()
+        menu_item = Menu_item.objects.values()
+
+        
+        pp(category)
+
+        data = {
+            'title':list(menu_item)[0]['title'],
+            'price':list(menu_item)[0]['price'],
+            'cuisine': list(cuisine.filter(id = 1)),
+            'category': list(category.filter(id = 2)),
+            'spicy_level':list(menu_item)[0]['spicy_level'],
+      
+        }
+
+        return JsonResponse({'data': data})
