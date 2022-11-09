0139de8bef1d8d2a93f435e9ac112776873999df
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-09 23:32:19 +0000

    JSON response

--- bistro/api/views.py
+++ bistro/api/views.py
@@ -7,21 +7,20 @@ from pprint import pprint as pp
 # Create your views here.
 
 data = ()
+
+
 def getData(request):
     menu_items = Menu_item.objects.all()
     JSON_DATA = list()
     if request.method == 'GET':
         for item in menu_items:
             JSON_DATA.append({
-                'title':item.title,
-                'price':item.price,
-                'decription':item.description,
-                'spicy_level':item.spicy_level,
-                'category':model_to_dict(Category.objects.get(id=item.category_id)),
-                'cuisine':model_to_dict(Cuisine().objects.get(id=item.cuisine_id)),
+                'title': item.title,
+                'price': item.price,
+                'decription': item.description,
+                'spicy_level': item.spicy_level,
+                'category': model_to_dict(Category.objects.get(id=item.category_id)),
+                'cuisine': model_to_dict(Cuisine.objects.get(id=item.cuisine_id)),
             })
 
-        return JsonResponse({'data':JSON_DATA})
-
-
-
+        return JsonResponse({'data': JSON_DATA})
