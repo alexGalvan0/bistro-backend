2bb1780709745f015868943b744e3a4e20fda471
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-10 18:52:40 +0000

    init

--- bistro/api/views.py
+++ bistro/api/views.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render
-from django.http import JsonResponse,HttpResponse
-from django.forms.models import model_to_dict
-from api.models import Menu_item, Category, Cuisine
+import json
 from pprint import pprint as pp
+
+import requests
+from django.forms.models import model_to_dict
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from api.models import Category, Cuisine, Menu_item
 
 # Create your views here.
 
 
 
 @require_http_methods(["GET"])
-def getFriendlys(request,id):
+def getFriendlys(request, id):
     JSON_DATA = list()
     if id != 0:
         menu_items = Menu_item.objects.filter(restaurant_id=id)
     elif id == 0:
-         menu_items = Menu_item.objects.all()
+        menu_items = Menu_item.objects.all()
 
     for item in menu_items:
+        response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?s={item.title.replace(" ","_")}').json()
+        pp(response['meals'][0]['strInstructions'])
         JSON_DATA.append({
             'title': item.title,
             'price': item.price,
@@ -26,5 +31,6 @@ def getFriendlys(request,id):
             'spicy_level': item.spicy_level,
             'category': model_to_dict(Category.objects.get(id=item.category_id)),
             'cuisine': model_to_dict(Cuisine.objects.get(id=item.cuisine_id)),
-        })        
-    return JsonResponse(JSON_DATA,safe=False)
+            
+        })
+    return JsonResponse(JSON_DATA, safe=False)
