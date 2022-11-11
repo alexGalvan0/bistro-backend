c7097677219c51c905b6ce3bafb849f342ac4aaa
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-11 13:44:35 +0000

    csv

--- bistro/api/urls.py
+++ bistro/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('json/<int:id>', views.getFriendlys, name='restaurant data'),
-   path('csv/',views.csvData,name='getcsv')
+   path('json/<int:id>', views.getFriendlys, name='restaurant data json'),
+   path('csv/<int:id>', views.csv_view, name='restaurant data csv'),
 ]
--- bistro/api/views.py
+++ bistro/api/views.py
@@ -10,8 +10,7 @@ from django.views.decorators.http import require_http_methods
 
 from api.models import Category, Cuisine, Menu_item
 
-# Create your views here.
-
+# Create your views here
 
 
 @require_http_methods(["GET"])
@@ -24,24 +23,25 @@ def getFriendlys(request, id):
         menu_items = Menu_item.objects.all()
 
     for item in menu_items:
-    
-        response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?s={item.title.replace(" ","_")}').json()
-        if item.description!= '':
+
+        response = requests.get(
+            f'http://www.themealdb.com/api/json/v1/1/search.php?s={item.title.replace(" ","_")}').json()
+        if item.description != '':
             desc = item.description
         else:
             desc = response['meals'][0]['strInstructions']
 
         JSON_DATA.append({
-            'id':item.id,
+            'id': item.id,
             'title': item.title,
             'price': item.price,
             'description': desc,
             'spicy_level': item.spicy_level,
             'category': model_to_dict(Category.objects.get(id=item.category_id)),
             'cuisine': model_to_dict(Cuisine.objects.get(id=item.cuisine_id)),
-            'image':response['meals'][0]['strMealThumb'],
-            'video':response['meals'][0]['strYoutube']
-            
+            'image': response['meals'][0]['strMealThumb'],
+            'video': response['meals'][0]['strYoutube']
+
         })
     if len(JSON_DATA) == 0:
         return HttpResponse('<h1 style="color:red">NO Response</h1>')
@@ -49,27 +49,21 @@ def getFriendlys(request, id):
         return JsonResponse(JSON_DATA, safe=False)
 
 
-@require_http_methods(["GET"])
-def csvData(request):
-    CSV_LIST = list()
+def csv_view(request, id):
+    if id != 0:
+        menu_items = Menu_item.objects.filter(restaurant_id=id)
+    elif id == 0:
+        menu_items = Menu_item.objects.all()
 
-    menu_items = Menu_item.objects.all()
-
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
+    )
+    writer = csv.writer(response)
+    writer.writerow(['title', 'price', 'description', 'spicy_level'])
     for item in menu_items:
-       
-        response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?s={item.title.replace(" ","_")}').json()
-        if item.description!= '':
-            desc = item.description
-        else:
-            desc = response['meals'][0]['strInstructions']
-\
-        response =  HttpResponse(
-            content_type = 'text/csv',
-            headers={'Content-Disposition': 'attachment; filename="menuItems.csv"'},
-        )
-        writer =  csv.writer(response)
-        writer.writerow(['id', 'title', 'price', 'description','spicy_level','category','cuisine','image','video'])
-        writer.writerow([item.id, item.title, item.price, item.description, '"Testing"', "Here's a quote", "Here's a quote", "Here's a quote", "Here's a quote"])
-        
-    return response
+        writer.writerow([item.title, item.price, item.description,
+                        item.spicy_level])
 
+    return response
